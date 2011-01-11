093d186cb457c2bbcf58b52da3f2ecd6dfaf8b26
Christian Hjalmarsson <christian@MacBook-Air.local>
2011-01-11 11:00:22 +0100

    Init of skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HjalmarssonNet::Application.config.secret_token = '6a3f7bd952599c3f8637f2283795f72360754ec9ba98c7b2fb8c0749f2fed4fa8ce49576c01bac324750d9f1eb6de8587276b0589e99943bfab35f465a57f0b8'
