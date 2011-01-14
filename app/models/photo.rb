5a22fd1a50ca5be38a9e3cdcb609014dfc1c6548
Christian Hjalmarsson <christian@MacBook-Air.local>
2011-01-14 08:41:17 +0100

    test

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,8 @@
 class Photo < ActiveRecord::Base
   has_attached_file :photo,
-    :styles => { :thumb => "150x150>" },
-    :storage => :s3, 
-    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", 
-    :path => "photos/:id/:style/:basename.:extension"
+    :styles => { 
+      :medium => "300x300>",
+      :thumb => "100x100>" 
+    },
+    :path => ":rails_root/public/assets/images/photos/:id/:style/:basename.:extension"
 end
